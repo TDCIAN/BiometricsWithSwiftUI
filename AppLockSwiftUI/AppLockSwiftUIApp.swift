514e8aa8b1397b4703c4aeb7a5494644a7590d61
Jeongmin <tdcian71@gmail.com>
2021-05-17 23:42:23 +0900

    Initial Commit

---
+++ AppLockSwiftUI/AppLockSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppLockSwiftUIApp.swift
+//  AppLockSwiftUI
+//
+//  Created by APPLE on 2021/05/17.
+//
+
+import SwiftUI
+
+@main
+struct AppLockSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
